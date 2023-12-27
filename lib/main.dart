1a56c04f24a0e3c1b6ded92c9759f43723e98e37
biamaia103 <bia.maia103@gmail.com>
2023-12-27 23:24:38 +0000

    começo

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
+import 'second_screen.dart';
+
 void initFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -22,8 +24,14 @@ void main() {
 
 
 class Localizacao{
-
+    var nome;
+    var descricao;
+    var latitude;
+    var longitude;
+    var imagemURL;
+    var estado;
 }
+
 class LocalInteresse{
 
 }
@@ -38,28 +46,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'JourneyBuddy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade900),
         useMaterial3: true,
       ),
       //home: const MyHomePage(title: 'JourneyBuddy'),
       initialRoute: MyHomePage.routeName,
       routes: {
-        MyHomePage.routeName: (context) => const MyHomePage(title: 'JourneyBuddy'),
+        MyHomePage.routeName: (context) => const MyHomePage(title: 'Localizações'),
         SecondScreen.routeName : (context) => const SecondScreen(),
       },
     );
@@ -71,37 +65,17 @@ class MyHomePage extends StatefulWidget {
 
   static const String routeName = '/';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<Localizacao>? _listaLocalizacoes;
+  List<Localizacao>? _listaLocalizacoes = [];
   bool _fetchingData = false;
-
-  Future<void> _fetchLocalizacoes() async{
-    try{
-
-    }
-    catch(ex){
-      debugPrint('Something went wrong: $ex');
-    }
-    finally{
-      setState(() => _fetchingData = false);
-    }
-  }
 
 
   @override
@@ -115,72 +89,126 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-  int _counter = 0;
+  // FIREBASE
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Future<void> _fetchLocalizacoes() async{
+      try{
+          _listaLocalizacoes!.clear();
+          var db = FirebaseFirestore.instance;
+          var collection = await db.collection('Localidades').get();
+          for(var doc in collection.docs) {
+              var l = new Localizacao();
+              l.nome = doc['nome'];
+              l.descricao = doc['descrição'];
+              l.latitude = (doc['coordenadas'] as GeoPoint).latitude;
+              l.longitude = (doc['coordenadas'] as GeoPoint).longitude;
+              l.imagemURL = doc['imagemURL'];
+              l.estado = doc['estado'];
+
+              _listaLocalizacoes!.add(l);
+          }
+      }
+      catch(ex){
+        debugPrint('Something went wrong: $ex');
+      }
+      finally{
+        setState(() => _fetchingData = false);
+      }
+    }
+
+    void listDocuments() async {
+        var db = FirebaseFirestore.instance;
+        var collection = await db.collection('Localidades').get();
+        for(var doc in collection.docs) {
+            var l = new Localizacao();
+            l.nome = doc['nome'];
+            l.descricao = doc['descrição'];
+            l.latitude = (doc['coordenadas'] as GeoPoint).latitude;
+            l.longitude = (doc['coordenadas'] as GeoPoint).longitude;
+            l.imagemURL = doc['imagemURL'];
+            l.estado = doc['estado'];
+
+            _listaLocalizacoes?.add(l);
+        }
+    }
+
+  // END FIREBASE
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+
+            if (_fetchingData) const CircularProgressIndicator(),
+
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              _listaLocalizacoes!.length.toString(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+
+            if (!_fetchingData && _listaLocalizacoes != null && _listaLocalizacoes!.isNotEmpty)
+              SizedBox(height: 200,
+                child: ListView.separated(
+                  itemCount: _listaLocalizacoes!.length,
+                  separatorBuilder: (_, __) => const Divider(thickness: 2.0),
+                  itemBuilder: (BuildContext context, int index) => Column(
+                    children: [
+                      Text(
+                        _listaLocalizacoes![index].nome,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Text(
+                        _listaLocalizacoes![index].descricao,
+                      ),
+                        Text(
+                            _listaLocalizacoes![index].latitude.toString(),
+                        ),
+                        Text(
+                            _listaLocalizacoes![index].longitude.toString(),
+                        ),
+                        Text(
+                            _listaLocalizacoes![index].imagemURL,
+                        ),
+                        Text(
+                            _listaLocalizacoes![index].estado,
+                        ),
+
+                    ],
+                  ),
+                ),
+              )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+
+      floatingActionButton : FloatingActionButton(
+        onPressed: () {
+            setState(() => _fetchingData = true);
+            _fetchLocalizacoes();
+        },
+        child: const Icon(Icons.refresh),
+      )
+      /*floatingActionButton: FloatingActionButton(
         onPressed: (){
-          Navigator.pushNamed(context, SecondScreen.routeName, arguments: _counter);
+          Navigator.pushNamed(context, SecondScreen.routeName, arguments: 69);
         },
         child: const Text('Locais de Interesse'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), */// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
